22a638d6da9788bab14772664c9197f8bd13e07d
Chandankrswain <mr.chandanswain@gmail.com>
2025-02-16 00:25:38 +0530

    ADD : Added the Logo inside the navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,5 +1,11 @@
+import Logo from "../../../public/logo.png";
+
 export const Navbar = () => {
-  return <div className="text-2xl">Navbar</div>;
+  return (
+    <div className="text-2xl h-20">
+      <img className="w-14 h-14 m-4" src={Logo} alt="" />
+    </div>
+  );
 };
 
 export default Navbar;
