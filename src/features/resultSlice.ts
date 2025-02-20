9c0b7891b0f627bc6952131bbf7395cd14693255
Chandankrswain <mr.chandanswain@gmail.com>
2025-02-20 23:55:33 +0530

    Changed the slice to show the result

--- src/features/resultSlice.ts
+++ src/features/resultSlice.ts
@@ -1,13 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface ResultState {
+  wpm: number;
+  errors: number;
+  correctChars: number;
+  totalTime: number;
+}
+
+const initialState: ResultState = {
+  wpm: 0,
+  errors: 0,
+  correctChars: 0,
+  totalTime: 10, // Default total time
+};
 
 export const resultSlice = createSlice({
   name: "result",
-  initialState: {
-    value: "",
+  initialState,
+  reducers: {
+    setWPM: (state) => {
+      const wpm = Math.round((state.correctChars / 5 / state.totalTime) * 60);
+      state.wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+    },
+    setErrors: (state, action: PayloadAction<number>) => {
+      state.errors = action.payload;
+    },
+    setCorrectChars: (state, action: PayloadAction<number>) => {
+      state.correctChars = action.payload;
+    },
+    setTotalTime: (state, action: PayloadAction<number>) => {
+      state.totalTime = action.payload;
+    },
   },
-  reducers: {},
 });
 
-export const {} = resultSlice.actions;
+export const { setWPM, setErrors, setCorrectChars, setTotalTime } =
+  resultSlice.actions;
 
 export default resultSlice.reducer;
