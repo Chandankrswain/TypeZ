d13f28c7a86b315d110d99e2c216ae218d5ff216
Chandankrswain <mr.chandanswain@gmail.com>
2025-02-23 00:42:00 +0530

    Caps lock Component done

--- src/components/capslock-indicator/capslock-indicator.tsx
+++ src/components/capslock-indicator/capslock-indicator.tsx
@@ -1,10 +1,10 @@
-import { VscLockSmall } from "react-icons/vsc";
+import { FaLock } from "react-icons/fa";
 
 export const CapslockIndicator = () => {
   return (
-    <div className="flex bg-[#bb86fc]">
-      <VscLockSmall />
-      <p>Caps Lock</p>
+    <div className="flex bg-[#bb86fc] justify-between items-center py-4 px-5 w-[170px] rounded-lg m-auto mb-2 ">
+      <FaLock className="w-5 h-5" />
+      <p className="text-md tracking-wide">Caps Lock</p>
     </div>
   );
 };
