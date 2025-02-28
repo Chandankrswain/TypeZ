485e0bcc06af13379e404cf1bab058c4338c5a69
chandankrswain <mr.chandanswain@gmail.com>
2025-03-01 00:16:36 +0530

    reset the changes

--- src/components/keyboard-layout/keyboard-layout.tsx
+++ src/components/keyboard-layout/keyboard-layout.tsx
@@ -19,7 +19,7 @@ export const KeyboardLayout = () => {
   useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
       const key = e.key.toLowerCase();
-      setPressedKey(key); //linux setup done
+      setPressedKey(key);
 
       // Check if the key pressed is correct
       const currentIndex = getUserInput.length;
