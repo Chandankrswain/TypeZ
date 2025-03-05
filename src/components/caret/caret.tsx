5020733df41aa636f4988c15f4c1465ee946a791
chandankrswain <mr.chandanswain@gmail.com>
2025-03-06 00:27:16 +0530

    ADD: completed the caret component

--- src/components/caret/caret.tsx
+++ src/components/caret/caret.tsx
@@ -1,5 +1,25 @@
-const caret = () => {
-  return <div>caret</div>;
+import { motion } from "framer-motion";
+
+const Caret = ({
+  position,
+  isFocused,
+}: {
+  position: number;
+  isFocused: boolean;
+}) => {
+  return (
+    <motion.div
+      animate={{
+        opacity: isFocused ? [1, 0, 1] : 0, // Blinking effect when focused
+        x: position * 18, // Adjust movement based on text position
+      }}
+      transition={{
+        opacity: { duration: 0.8, repeat: Infinity },
+        x: { type: "spring", stiffness: 100 },
+      }}
+      className="absolute top-0 left-0 h-10 w-1 bg-[#bb86fc] rounded-sm"
+    />
+  );
 };
 
-export default caret;
+export default Caret;
